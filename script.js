b5ce209c2d652aaa9d7a78f8e64c7c08c8bd4b10
Stephen <stephensouness@outlook.com>
2023-10-25 10:38:10 +0100

    created a createPlayer function, for playerName and type

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const container = document.querySelector(".container");
 const squares = document.querySelectorAll(".square");
 
-const gameBoardArr = ["O", "X", "X", "O", "O", "X", "X", "O", "X"];
-
-gameBoardArr.forEach((item, i) => {
-  squares[i].textContent = item;
-});
-
 // const gameBoard = {
 //   gameBoardArr: ["O", "X", "X", "O", "O", "X", "X", "O", "X"],
 // };
 
-const playerOne = {
-  name: "Isla",
-  marker: "X",
-};
+const gameBoardArr = [];
 
-const playerTwo = {
-  name: "Ava",
-  marker: "O",
-};
+function createPlayer(name, type) {
+  const newPlayer = {};
+  newPlayer.userName = name;
+  newPlayer.type = type;
+  return newPlayer;
+}
+
+const playerOne = createPlayer("Player One", "O");
+const playerTwo = createPlayer("Player Two", "X");
+
+console.log(playerOne);
+console.log(playerTwo);
+
+function addToBoard(item) {
+  this.gameBoardArr.push(item);
+}
+
+// playerOne.addToBoard("X");
+
+gameBoardArr.push("X");
+gameBoardArr.push("O");
+gameBoardArr.push("X");
+
+console.log(gameBoardArr);
+
+gameBoardArr.forEach((item, i) => {
+  squares[i].textContent = item;
+});
