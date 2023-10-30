02385e53b14eddf8f02d567e4030c9f9dbd3058f
Stephen <stephensouness@outlook.com>
2023-10-30 13:13:30 +0000

    the index gameBoardArr will now popululate with X's in the correct nested index once a specific square is clicked

--- script.js
+++ script.js
@@ -3,59 +3,80 @@ const squares = document.querySelectorAll(".square");
 
 // Create Player Object (Factory)
 
-function createPlayer(name, type, score) {
+function createPlayer(name, type) {
   const newPlayer = {};
   newPlayer.name = name;
   newPlayer.type = type;
-  newPlayer.score = score;
   return newPlayer;
 }
 
-const playerOne = createPlayer("Player One", "0", 0);
-const playerTwo = createPlayer("Player Two", "X", 0);
+const playerOne = createPlayer("Player One", "0");
+const playerTwo = createPlayer("Player Two", "X");
 
 // GameBoard (IIFE Module)
 
 const GameBoard = (function () {
-  const gameBoardArr = [];
+  const gameBoardArr = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
 
-  const addToBoard = function (choice, i) {
+  const addToBoard = function (choice, index) {
     gameBoardArr.push(choice);
     gameBoardArr.forEach((item) => {
-      squares[i].textContent = item;
+      squares[index].textContent = item;
     });
   };
-  return { gameBoardArr, addToBoard };
+
+  return { addToBoard, gameBoardArr };
 })();
 
-GameBoard.addToBoard("X", 6);
-GameBoard.addToBoard("0", 7);
-GameBoard.addToBoard("X", 2);
-GameBoard.addToBoard("0", 4);
-GameBoard.addToBoard(playerOne.type, 5);
-GameBoard.addToBoard(playerTwo.type, 8);
+// const winner = function () {
+//   if (
+//     testGameBoardArr[0][0] === "X" &&
+//     testGameBoardArr[0][1] === "X" &&
+//     testGameBoardArr[0][2] === "X"
+//   ) {
+//     console.log("Well done you are the winner!");
+//   }
+// };
 
-console.log(GameBoard.gameBoardArr);
+// winner();
 
-// Square event listener functionality//////////////////////////////////////////////////////////
+const testGameBoardArr = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+];
 
-// squares.forEach((square) => {
-//   square.addEventListener("click", () => {
-//     if (gameBoardArr[gameBoardArr.length - 1] === playerOne.type) {
-//       gameBoardArr.push(playerTwo.type);
-//       square.innerText = playerTwo.type;
-//       console.log(gameBoardArr);
-//     } else {
-//       gameBoardArr.push(playerOne.type);
-//       square.innerText = playerOne.type;
-//       console.log(gameBoardArr);
-//     }
-//   });
-// });
+let squareID = "";
 
-// console.log(gameBoardArr[gameBoardArr.length - 1]);
-//////////////////////////////////////////////////////////////////////////////////////////////////
+squares.forEach((square) => {
+  square.addEventListener("click", (event) => {
+    square.innerText = "X";
+    squareID = event.target.id;
 
-// NEED TO EVENTUALLY CREATE A NESTED ARRAY AND CORRESPONDING OBJECT?
-// const gameBoardArr = [[], [], []];
-// then perhaps an object to fill this?
+    addToArray(squareID);
+  });
+});
+
+function addToArray(squareID) {
+  let indexObj = {
+    square0: [0, 0],
+    square1: [0, 1],
+    square2: [0, 2],
+    square3: [1, 0],
+    square4: [1, 1],
+    square5: [1, 2],
+    square6: [2, 0],
+    square7: [2, 1],
+    square8: [2, 2],
+  };
+
+  const indices = indexObj[squareID];
+  if (indices) {
+    testGameBoardArr[indices[0]][indices[1]] = "X";
+    console.log(testGameBoardArr);
+  }
+}
