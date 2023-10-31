7896612f18a628306ae6d46da4b3636376523686
Stephen <stephensouness@outlook.com>
2023-10-31 08:53:06 +0000

    changed unique square ID to single number to make winning combo function easier

--- script.js
+++ script.js
@@ -34,30 +34,30 @@ const GameBoard = (function () {
         square.innerText = playerOne.type;
         squareID = event.target.id;
         addToArray(squareID, player);
-
         turn = 1;
+        winner(squareID);
       } else if (turn === 1 && square.innerText === "") {
         player = playerTwo.type;
         square.innerText = playerTwo.type;
         squareID = event.target.id;
         addToArray(squareID, player);
-
         turn = 0;
+        winner(squareID);
       }
     });
   });
 
   function addToArray(squareID) {
     let indexObj = {
-      square0: [0, 0],
-      square1: [0, 1],
-      square2: [0, 2],
-      square3: [1, 0],
-      square4: [1, 1],
-      square5: [1, 2],
-      square6: [2, 0],
-      square7: [2, 1],
-      square8: [2, 2],
+      0: [0, 0],
+      1: [0, 1],
+      2: [0, 2],
+      3: [1, 0],
+      4: [1, 1],
+      5: [1, 2],
+      6: [2, 0],
+      7: [2, 1],
+      8: [2, 2],
     };
 
     const indices = indexObj[squareID];
@@ -65,11 +65,9 @@ const GameBoard = (function () {
       if (player === playerOne.type) {
         gameBoardArr[indices[0]][indices[1]] = "0";
         console.log(gameBoardArr);
-        winner();
       } else if (player === playerTwo.type) {
         gameBoardArr[indices[0]][indices[1]] = "X";
         console.log(gameBoardArr);
-        winner();
       }
     }
   }
